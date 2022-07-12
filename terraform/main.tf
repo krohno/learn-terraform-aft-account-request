3de8d837c43f7b89a623461b501d8cf41096db32
Adam Krohn <akrohn@fortinet.com>
2022-07-12 13:30:42 -0400

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "fti_aft_test_1@lab.training.fortinet.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "fti_aft_test_1@lab.training.fortinet.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
